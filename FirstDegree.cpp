d8e0a1f8508f623075ef26148fdffa64f136e3c3
Sergey_Ryazanov <majo78@mail.ru>
2021-09-17 21:46:35 +0300

    Changes made in file FirstDegree.cpp

--- FirstDegree.cpp
+++ FirstDegree.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 int main() {
-std::cout << "Программа выводит N первых степеней числа K\n";
-int k, n;
-std::cout << "Введите число K ---> ";
-std::cin >> k;
-std::cout << "Введите число первых степеней N ---> ";
-std::cin >> n;
-int m = k;
-int count = 0;
-    do {
-        std::cout << m << "\n";
-        count++;
-    } while (count <= m);
+    std::cout << "Программа выводит N степеней числа K\n";
+    int k, n;
+    std::cout << "Введите число K ---> ";
+    std::cin >> k;
+    std::cout << "Введите степень числа N ---> ";
+    std::cin >> n;
+    int result = 1;
+    int count = 0;
+       do {
+           result *= k;
+           ++count;
+              if (count == n) std::cout << "Число " << n << " в степени " << k << " = " << result << "\n";
+       } while (count < n);
 }
