ce60d4bccff7b929153709eea9f0357748fe0e3b
Sergey Ryazanov <90528778+majotraderluky@users.noreply.github.com>
2021-09-25 14:45:09 +0300

    Add file SecondDegree.cpp

---
+++ SecondDegree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Введите целое число ---> ";
+    int number;
+    std::cin >> number;
+       do {
+           if (number % 2 == 0) {
+               number /= 2;
+           } else {
+               break;
+           }
+       } while (number > 0);
+      std::cout << "number = " << number << "\n";
+           if (number == 1) {
+               std::cout << "Число является степенью двойки\n";
+           } else {
+               std::cout << "Число не является степенью двойки\n";
+           }
+}
