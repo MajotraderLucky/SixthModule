748e398c91c23c2239f7216ea735bbdd1230fe4b
Sergey Ryazanov <90528778+majotraderluky@users.noreply.github.com>
2021-09-25 18:09:31 +0300

    Add file FiboNumbers.cpp

---
+++ FiboNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int count;
+    int f, f1 = -1, f2 = 1;
+    std::cout << "Введите порядок числа фибоначчи ---> ";
+    std::cin >> count;
+       do {
+           f = f1 + f2;
+           f1 = f2;
+           f2 = f;
+           count--;
+       } while (count > -1);
+       std::cout << f << "\n";
+}
